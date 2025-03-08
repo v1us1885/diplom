551c2088fd3747ba02a3387ccb6a38438000285e
V1us1885 <v1rus1885@mail.ru>
2025-03-08 11:44:46 +0000

    Добавлена конфигурация провайдера Yandex в terraform-mks

--- sa-backet/main.tf
+++ sa-backet/main.tf
@@ -18,6 +18,15 @@ resource "yandex_resourcemanager_folder_iam_binding" "storage_editor" {
   ]
 }
 
+# Назначение роли editor сервисному аккаунту (для инициализации terraform)
+resource "yandex_resourcemanager_folder_iam_binding" "editor" {
+  folder_id = var.yc_folder_id
+  role      = "editor"
+  members   = [
+    "serviceAccount:${yandex_iam_service_account.terraform.id}"
+  ]
+}
+
 # Создание статического ключа доступа
 resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
   service_account_id = yandex_iam_service_account.terraform.id
